812c8e6b46e15519cb75b8b305029675ec65ec37
Elias Naur <mail@eliasnaur.com>
2023-04-08 12:55:40 -0600

    gcc: tighten platform flags special-case for aarch64-darwin
    The 4aa95e33121c316c5b34031bb08106d2dc113e38 commit added support for
    aarch64-darwin but also ignored platform flags if the build platform
    is aarch64-darwin. This leads to confusing errors such as
    `pkgsCross.raspberryPi` packages compiled with soft-float even though
    the platform supports hard-float (and is built as such on other
    platforms).
    
    The correct way to ignore platform flags is to check `targetPlatform`,
    not the build platform. This change fixes that.
    
    While we're here, tigthen the special-case to cover only the problematic
    flags: `-with-cpu` and `-with-arch`.

--- pkgs/development/compilers/gcc/common/platform-flags.nix
+++ pkgs/development/compilers/gcc/common/platform-flags.nix
@@ -1,11 +1,14 @@
 { lib, targetPlatform }:
 
 let
+  isAarch64Darwin = targetPlatform.isDarwin && targetPlatform.isAarch64;
   p =  targetPlatform.gcc or {}
     // targetPlatform.parsed.abi;
 in lib.concatLists [
-  (lib.optional (!targetPlatform.isx86_64 && p ? arch) "--with-arch=${p.arch}") # --with-arch= is unknown flag on x86_64
-  (lib.optional (p ? cpu) "--with-cpu=${p.cpu}")
+  # --with-arch= is unknown flag on x86_64 and aarch64-darwin.
+  (lib.optional (!targetPlatform.isx86_64 && !isAarch64Darwin && p ? arch) "--with-arch=${p.arch}")
+  # Skip `--with-cpu` on aarch64-darwin; otherwise configure fails with "Unknown cpu used in --with-cpu=apple-a13".
+  (lib.optional (!isAarch64Darwin && p ? cpu) "--with-cpu=${p.cpu}")
   (lib.optional (p ? abi) "--with-abi=${p.abi}")
   (lib.optional (p ? fpu) "--with-fpu=${p.fpu}")
   (lib.optional (p ? float) "--with-float=${p.float}")
